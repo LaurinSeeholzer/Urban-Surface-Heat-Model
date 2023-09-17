dd7433195c1ae4d99dd5a6bf28ceefd635bb4af4
Laurin Seeholzer <laurin@nirual-solutions.ch>
2023-09-17 20:52:23 +0200

    browser routing

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -1,10 +1,15 @@
 import { ArrowSmallRightIcon } from '@heroicons/react/24/outline';
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const LandingPage = () => {
   return (
-    <div className='bg-black w-full'>
-idk
+    <div className='bg-gray-50 w-full'>
+        <Link to='/Urban-Surface-Heat-Model/dashboard'>
+            <a href='/Urban-Surface-Heat-Model/dashboard'>
+                here
+        </a>
+                                </Link>
     </div>
   );
 }
