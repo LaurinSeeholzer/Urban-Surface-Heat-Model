3207edd6bf7426445909d38dd3edcb07ed22f090
Laurin Seeholzer <laurin@nirual-solutions.ch>
2023-09-17 20:06:15 +0200

    browser routing

--- src/index.js
+++ src/index.js
@@ -6,9 +6,7 @@ import { BrowserRouter, HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <HashRouter>
+  <BrowserRouter>
     <App />
-    </HashRouter>
-  </React.StrictMode>
+  </BrowserRouter>
 );
